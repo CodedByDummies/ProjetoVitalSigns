69d78a4a193ce997d9a5f9b05f9ddabcf14186de
Kelvin Soares <Kelvin_soares@outlook.com>
2018-10-06 14:37:35 -0300

    Alterações KELVIN

---
+++ VitalSigns/VitalSigns/UI/Localizar Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VitalSigns.UI
+{
+    public partial class Localizar_Cliente : Form
+    {
+        public Localizar_Cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
