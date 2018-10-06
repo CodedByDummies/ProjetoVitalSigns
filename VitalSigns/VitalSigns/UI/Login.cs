14ff9bfdc31ca9f25f6edcbc6fcc1f8c721bcf73
Clayton Dias <clayton.dias.sousa@hotmail.com>
2018-10-06 14:47:12 -0300

    login

--- VitalSigns/VitalSigns/UI/Login.cs
+++ VitalSigns/VitalSigns/UI/Login.cs
@@ -16,5 +16,15 @@ namespace VitalSigns.UI
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
